a687b9b456aefcb57980169a99800ac97daa2c95
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-01-24 17:03:14 +0200

    wtwtgg

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,7 +1,24 @@
 import {Component} from "react";
 
+import styles from "./phonebook.module.scss"
 class Phonebook extends Component {
   render() {
+    return(
+      <div>
+        <h2>Phonebook</h2>
+        <div className={styles.wrapper}>
+          <div className={styles.block}>
+            <form>
+              <div className={styles.formGroup}>
+                <label>Name</label>
+                <input placeholder='name'/>
+              </div>
+            </form>
+          </div>
+          <div className={styles.block}></div>
+        </div>
+      </div>
+    )
   }
 }
 export default Phonebook
